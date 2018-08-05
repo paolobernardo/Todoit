8f3360186f18e5afb88f13bcb7a820bd73104cfd
Paolo Bernardo <PaoloB@paolos-macbook-pro.local>
2018-08-05 21:48:02 +1200

    Add new item functionality complete

--- Todoit/TodoListViewController.swift
+++ Todoit/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Do Shopping", "Get Helmet", "Buy Flats"]
+    var itemArray = ["Do Shopping", "Get Helmet", "Buy Flats"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +54,33 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    
+    //MARK - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New To-do List Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add item button on our UIalert
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 
 }
 
